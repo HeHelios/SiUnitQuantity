821506f4c226fdbff0fcc3dcb327154cc0d017c6
Serhii Kryhin <55597550+HeHelios@users.noreply.github.com>
2020-07-07 15:00:48 +0300

    Added __str__ method

--- natural.py
+++ natural.py
@@ -1,5 +1,5 @@
 class SiUnitQuantity:
-    def __init__(self, mass_exp = 0, len_exp = 0, time_exp = 0, curr_exp = 0, temp_exp = 0, magnitude = 1.):
+    def __init__(self, magnitude = 1.0, mass_exp = 0, len_exp = 0, time_exp = 0, curr_exp = 0, temp_exp = 0):
         self.mass_exp = mass_exp
         self.len_exp = len_exp
         self.time_exp = time_exp
@@ -11,10 +11,68 @@ class SiUnitQuantity:
         return self.mass_exp == self.len_exp == self.time_exp == self.curr_exp == self.temp_exp == 0
 
     def match_units(self, other):
-        return self.mass_exp == other_mass_exp and self.len_exp == other.len_exp and self.time_exp == other.time_exp and self.curr_exp == other.curr_exp and self.temp_exp == other.temp_exp
+        return self.mass_exp == other.mass_exp and self.len_exp == other.len_exp and self.time_exp == other.time_exp and self.curr_exp == other.curr_exp and self.temp_exp == other.temp_exp
 
     def __str__(self):
+        result = str(self.magnitude) + ' '
         
+        numerator = ''
+        denominator = ''        
+        numerator_num = 0
+        denominator_num = 0
+        
+        if self.is_unitless():
+            return result[:-1]
+
+        #Helper functionfor cool output
+        def out_func(unit_exp, name):
+            nonlocal numerator, denominator, numerator_num, denominator_num
+            
+            if unit_exp > 0:
+                if unit_exp == 1:
+                    numerator += name + ' * '
+                else:
+                    numerator += name + '^' + str(unit_exp) + ' * '
+                numerator_num += 1
+            elif unit_exp < 0:
+                if unit_exp == -1:
+                    denominator += name + ' * '
+                else:
+                    denominator += name + '^' + str(-unit_exp) + ' * '
+                denominator_num += 1
+
+        
+        out_func(self.mass_exp, 'kg')
+        out_func(self.len_exp, 'm')
+        out_func(self.time_exp, 's')
+        out_func(self.curr_exp, 'A')
+        out_func(self.temp_exp, 'K')
+
+        
+        if numerator_num == 0:
+            numerator = '1'
+        elif numerator_num == 1:
+            numerator = numerator[:-3]
+            if denominator_num == 0:
+                return result + numerator
+            
+        else:
+            numerator = numerator[:-3]
+            if denominator_num != 0:
+                numerator = '(' + numerator + ')'
+            else:
+                return result + numerator
+            
+
+        if denominator_num == 1:
+            denominator = denominator[:-3]
+        else:
+            denominator = denominator[:-3]
+            denominator = '(' + denominator + ')'
+            
+        result += numerator + '/' + denominator
+        return result
+
 
     def __add__(self, right):
         if not isinstance(right, SiUnitQuantity):
@@ -22,9 +80,9 @@ class SiUnitQuantity:
                 return SiUnitQuantity(magnitude = self.magnitude + right)
             else:
                 raise TypeError("Unit mismatch in adding SiUnitQuantity and a non-SiUnitQuantity")
-        if not self.match_units(other):
+        if not self.match_units(right):
             raise TypeError("Unit mismatch in adding two SiUnitQuantities")
-        return SiUnitQuantity(mass_exp = self.mass_exp, len_exp = self.len_exp, time_exp = self.time_exp, curr_exp = self.curr_exp, temp_exp = self.temp_exp, magnitude = self.magnitude + other.magnitude)
+        return SiUnitQuantity(magnitude = self.magnitude + right.magnitude, mass_exp = self.mass_exp, len_exp = self.len_exp, time_exp = self.time_exp, curr_exp = self.curr_exp, temp_exp = self.temp_exp)
 
     def __radd__(self, right):
         return self + right
@@ -40,3 +98,11 @@ class SiUnitQuantity:
 
   #  def __rtruediv__(self, right):
   #      return Div(right, self)
+
+
+
+if __name__ == '__main__':
+    x = SiUnitQuantity(5, mass_exp = 1, len_exp = 2, time_exp = -2, temp_exp = -1)
+    y = SiUnitQuantity(10, len_exp = 1, time_exp = -1)
+    
+    print(x)
