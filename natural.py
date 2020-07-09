3004962a1ff82a599516418a3b2837ad25919f36
Serhii Kryhin <55597550+HeHelios@users.noreply.github.com>
2020-07-10 01:10:40 +0300

    Added premade objects and constants
    1) Added premade objects for basic SI units and dependent SI units
    
    2) Added physical constants

--- natural.py
+++ natural.py
@@ -1,6 +1,60 @@
-class Constants:
-    SI_BASIC_UNITS = {"mass": "kg", "length": "m", "time": "s", "current": "A", "temperature": "K"}
+from math import pi
 
+class Units:
+    #Basic
+    kg = SiUnitQuantity(magnitude = 1, exponents = {"mass": 1})
+    m = SiUnitQuantity(magnitude = 1, exponents = {"length": 1})
+    s = SiUnitQuantity(magnitude = 1, exponents = {"time": 1})
+    A = SiUnitQuantity(magnitude = 1, exponents = {"current": 1})
+    K = SiUnitQuantity(magnitude = 1, exponents = {"temperature": 1})
+    
+    #Dependent
+    Hz = 1/s
+    N = kg * m / (s**2)
+    J = N * m
+    W = J / s
+    Pa = N / (m**2)
+    C = A * s
+    V = J / C
+    F = C / V
+    Ohm = V / A
+    T = V * s / (m**2)
+    H = Ohm * s
+
+    c = 299792458 * m / s                              #speed of ligtht
+    h = 6.62607015e-34 * J * s                         #Planck constant
+    hbar = h/(2 * pi)                                  #Planck constant
+    G = 6.67430e-11 * J * m / (kg**2)                  #gravity constant
+    eps0 = 8.8541878128e-12 * F / m                    #vacuum electric permittivity
+    mu0 = 1.25663706212e-6 * N / (A**2)                #vacuum magnetic permeability
+    e = 1.602176634e-19 * C                            #elementary charge
+    #Na =                                              #Avogadro constant
+    kb = 1.380649e-23 * J / K                          #Boltzman constant    
+    alph = 7.2973525693e-3                             #fine structure constant
+    me = 9.1093837015e-31 * kg                         #electron mass
+    mp = 1.67262192369e-27 * kg                        #proton mass
+    mn = 1.67492749804e-27 * kg                        #neutron mass
+    sigm = 5.670374419e-8 * W / ( (m**2) * (K**4) )    #Stefan-Boltzman constant
+
+
+class Constants:
+    
+    #For user guide
+    SI_BASIC_UNITS = {"mass": "kg", "length": "m", "time": "s", "current": "A", "temperature": "K"}
+    
+    SI_DEPENDENT_UNITS = {"frequency": "Hz", "Force": "N", "energy": "J", "power": "W", "pressure": "Pa", \
+                          "charge": "C", "voltage": "V", "capacitance": "F", "resistance": "Ohm", \
+                          "Magnetic field": "T", "inductance": "H", }
+
+    #For internal code
+    __SI_BASIC_UNITS = {"kg": Units.kg, "m": Units.m, "s": Units.s, "A": Units.A, "K": Units.K}
+    
+    __SI_DEPENDENT_UNITS = {"Hz": Units.Hz, "N": Units.N, "J": Units.J, "W": Units.W, "Pa": Units.Pa, \
+                          "C": Units.C, "V": Units.V, "F": Units.F, "Ohm": Units.Ohm, \
+                          "T": Units.T, "H": Units.H, }
+    
+    
+    
 class SiUnitQuantity:
     def __init__(self, magnitude = 1.0, exponents = {"mass": 0, "length": 0, "time": 0, "current": 0, "temperature": 0}):
         self.exponents = exponents
@@ -206,9 +260,12 @@ class SiUnitQuantity:
     def __round__(self, digits = 0):
         return SiUnitQuantity(magnitude = round(self.magnitude, digits), exponents = self.exponents)
     
-    def int_units(self):    #Converts float exponents to int expotnents
+    def int_units(self):    
+    #Converts float exponents to int expotnents
         new_exponents = {key : int(self.exponents[key]) for key in self.exponents.keys()}
         return SiUnitQuantity(magnitude = self.magnitude, exponents = new_exponents)
+
+
 
 if __name__ == '__main__':
  
